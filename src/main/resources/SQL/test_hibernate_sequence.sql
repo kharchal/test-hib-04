46b0159a91eb3118348d58c684b732f2f95875e4
Alex <hav@ukr.net>
2016-11-29 15:20:31 +0200

    sql scripts were added

---
+++ src/main/resources/SQL/test_hibernate_sequence.sql
@@ -0,0 +1,1 @@
+INSERT INTO test.hibernate_sequence (next_val) VALUES (16);
---
+++ src/main/resources/SQL/test_pets.sql
@@ -0,0 +1,8 @@
+INSERT INTO test.pets (name, note) VALUES ('Tiki', null);
+INSERT INTO test.pets (name, note) VALUES ('Bella', null);
+INSERT INTO test.pets (name, note) VALUES ('Gera', '');
+INSERT INTO test.pets (name, note) VALUES ('Sasha', '');
+INSERT INTO test.pets (name, note) VALUES ('Chiki', '');
+INSERT INTO test.pets (name, note) VALUES ('Funtikss', '');
+INSERT INTO test.pets (name, note) VALUES ('Rockie', '');
+INSERT INTO test.pets (name, note) VALUES ('Name', '');
